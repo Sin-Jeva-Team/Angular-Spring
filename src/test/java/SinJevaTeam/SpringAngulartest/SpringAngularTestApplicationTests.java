9d579cd8ce2f3183069450ee562630b83474be35
Nerodacles <juan.manuel.canahuare@gmail.com>
2020-12-06 15:22:33 -0400

    angular-maven-install :+1:

---
+++ src/test/java/SinJevaTeam/SpringAngulartest/SpringAngularTestApplicationTests.java
@@ -0,0 +1,13 @@
+package SinJevaTeam.SpringAngulartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
