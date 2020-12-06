fbf6487cc47fc55ee114c19867aa01a77cadfb42
Nerodacles <juan.manuel.canahuare@gmail.com>
2020-12-06 15:31:41 -0400

    Revert "asd"
    This reverts commit 9fe0ee5c26ee9cc784c94d23063eb151e2a16e6f.

---
+++ src/main/java/SinJevaTeam/SpringAngulartest/SpringAngularTestApplication.java
@@ -0,0 +1,13 @@
+package SinJevaTeam.SpringAngulartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularTestApplication.class, args);
+	}
+
+}
